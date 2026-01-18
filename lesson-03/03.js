92368b6f90cbcdf40866b134df2b632bdac3d379
Nurmatik <nur12345@internet.ru>
2026-01-19 04:59:48 +0600

    hw03-03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,15 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+	let number = num1;
+	if(number < num2){
+		number = num2;
+	}
+
+	if(number < num3){
+		number = num3;
+	}
+	return number;
+}
+
+console.log(findLargest(5, 7, 3));
