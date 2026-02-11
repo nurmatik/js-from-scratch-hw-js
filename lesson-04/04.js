d13f3fb492e252ff1cca9d059da2ce953e9e0e4e
Nurmatik <nur12345@internet.ru>
2026-02-11 23:49:09 +0600

    hw04-04

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -12,4 +12,8 @@
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(str) {
+	return str + str;
+}
+
+console.log(doubleEachCharacter('hello'));
