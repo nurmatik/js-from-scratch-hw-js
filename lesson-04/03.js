eadf64568d1e6dbb06b805e8238f32ff5ddcbb7b
Nurmatik <nur12345@internet.ru>
2026-02-11 23:36:02 +0600

    hw04-03

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,6 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(array1, array2) {
+	return array1.filter(element => includesElement(array2, element));
+}
