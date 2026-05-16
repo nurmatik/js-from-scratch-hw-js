bc1466d1e7e9765ba733bc3aff1ef4b6f16b36c6
Nurmatik <nur12345@internet.ru>
2026-05-16 12:53:11 +0600

    hw08

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,19 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+
+
+function capitalizeWords(str) {
+  if (!str) return str;
+  const words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    
+    if (word.length > 0) {
+      words[i] = word[0].toUpperCase() + word.slice(1);
+    }
+  }
+
+  return words.join(' ');
+}
